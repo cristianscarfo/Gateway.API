f1b2d039a91bd45cce12e63de690c0eda62c62f9
cristian <cristian.scarfo@hotmail.com>
2022-01-20 12:21:52 -0300

    Add project files.

---
+++ Gateway.API/Program.cs
@@ -0,0 +1,41 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+var hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        webBuilder.ConfigureAppConfiguration(config =>
+            config.AddJsonFile($"ocelot.{env}.json"));
+    });
+
+hostBuilder.Build().Run();
+
+app.UseOcelot().Wait();
